f539a075181f11927b23b0c8614522497d393622
juan <juan@DESKTOP-UMCEALU>
2023-02-18 20:17:52 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1;
+using System.Diagnostics.Contracts;
+
+Contacto1 x;
+x = new Contacto1();
+Console.WriteLine("Nombre");
+x.Nombre = Console.ReadLine();
+Console.WriteLine("Telefono");
+x.Telefono = Console.ReadLine();
+x.FechaNacimiento2 = DateTime.Parse("2003/01/01");
+
+Console.WriteLine(x.ToString());
+
+
+Console.ReadKey();
---
+++ ConsoleApp1/persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class persona
+    {
+        protected String nombre;
+        protected DateTime? fechaNacimiento;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public DateTime? FechaNacimiento2
+        {
+            get { return fechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                int edad;
+                edad = (DateTime.Now.Year - fechaNacimiento.Value.Year);
+                return edad;
+            }
+
+        }
+        public persona()
+        {
+            nombre = String.Empty;
+            fechaNacimiento = null;
+        }
+        public persona(String nombre, DateTime? fechaNacimiento)
+        {
+            this.nombre = nombre;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        public override string ToString()
+        {
+            return nombre.ToUpper() + " " + Edad;
+        }
+    }
+}
